0eef4ae2d036596c58659bcc8c4e1d794545d73e
jamesbeedy <jamesbeedy@gmail.com>
2019-04-16 19:10:19 +0000

    general cleanup
    * remove uneeded Makefile and tox.ini
    * use layer status
    * typos

--- Makefile
+++
@@ -1,9 +0,0 @@
-.PHONY: test
-test:
-	@tox
-
-build:
-	@charm build -rl DEBUG
-
-push:
-	@charm push `echo $(JUJU_REPOSITORY)`/builds/elasticsearch-base cs:~peopledatalabs/elasticsearch-base --resource elasticsearch-deb=/home/bdx/elasticsearch.deb
--- reactive/elastic_base.py
+++ reactive/elastic_base.py
@@ -13,12 +13,12 @@ from charms.reactive import (
 )
 
 from charmhelpers.core.hookenv import (
-    config,
     resource_get,
-    status_set,
 )
 
 from charmhelpers.core.host import is_container
+
+from charms.layer import status
 
 import charms.apt
 
@@ -26,19 +26,20 @@ import charms.apt
 ELASTIC_PKG = options.get('elastic-base', 'elastic-pkg')
 
 
-@when_not('elastic.pkg.set')
+@when_not('elastic.pkg.available')
 def check_elastic_pkg_layer_option():
     if ELASTIC_PKG:
-        set_flag('elastic.pkg.set')
+        set_flag('elastic.pkg.available')
     else:
-        status_set('blocked',
-                   'elastic-base layer config for elastic-pkg not set.')
+        status.blocked('elastic-base layer config for elastic-pkg not set.')
+        return
 
 
 # Install/Init ops
 # We have java, and know what elastic pkg to install, so lets get to it
-@when('apt.installed.openjdk-8-jre-headless', 'elastic.pkg.set')
-@when_not('elastic.pkg.available')
+@when('elastic.pkg.available',
+      'apt.installed.openjdk-8-jre-headless')
+@when_not('elastic.pkg.installed.available')
 def install_elastic_pkg():
     """Check for container, install elastic pkg
     from either apt or supplied resource .deb.
@@ -52,26 +53,22 @@ def install_elastic_pkg():
     # https://github.com/elastic/elasticsearch/commit/32df032c5944326e351a7910a877d1992563f791
     if is_container() and (ELASTIC_PKG == "elasticsearch"):
         os.environ['ES_SKIP_SET_KERNEL_PARAMETERS'] = 'true'
-        status_set('maintenance',
-                   "Installing in container based system")
+        status.maint('Installing in container based system')
 
     elastic_deb = resource_get('elastic-deb')
     if os.stat(elastic_deb).st_size > 0:
-        status_set('maintenance',
-                   "Installing {} from supplied .deb resource".format(
-                       ELASTIC_PKG.capitalize()))
+        status.maint(f'Installing {ELASTIC_PKG} from supplied .deb resource')
         sp.call(['dpkg', '-i', elastic_deb])
         set_flag('deb.installed.{}'.format(ELASTIC_PKG))
     else:
-        status_set('maintenance',
-                   "Installing {} from elastic.co apt repos".format(
-                       ELASTIC_PKG.capitalize()))
+        status.maint('Installing {ELASTIC_PKG} from elastic.co apt repos')
         charms.apt.queue_install([ELASTIC_PKG])
-    set_flag('elastic.pkg.available')
+    set_flag('elastic.pkg.installed.available')
 
 
 @when_any('deb.installed.{}'.format(ELASTIC_PKG),
           'apt.installed.{}'.format(ELASTIC_PKG))
 @when_not('elastic.base.available')
 def set_elastic_base_available():
+    status.active('{ELASTIC_PKG} installed.')
     set_flag('elastic.base.available')
